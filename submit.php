6d25a55df7a0f7108cc2a07b1afbaaa797697e70
Jason Choi <63834270+jasonDBA@users.noreply.github.com>
2020-05-30 23:55:16 -0400

    profile website

---
+++ submit.php
@@ -0,0 +1,13 @@
+<html>
+  <body>
+    Thank you, <?php echo $_GET['fName']; $_GET['lName'];?>
+    <br>
+    Confirmed your request with the following information:
+    <br>
+    Full Name: <?php echo $_GET['fName']; $_GET['lName'];?><br>
+    Country: <?php echo $_GET['country']; ?><br>
+    Email: <?php echo $_GET['email'];?><br>
+    <br>
+    I will contact you sooner or later! Can't wait for our fun coffee time!
+  </body>
+</html>
